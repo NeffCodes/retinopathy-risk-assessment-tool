6b3d088ae414436dae3b2c0d559e16e73cbf640d
Sumi Nia Means <suminmeans@gmail.com>
2024-08-18 17:26:23 -0400

    Create and Define patients 'model'

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100) #stores the patient's name up to 100 char max
+    dob = models.DateField() #Patient's DOB
+    history = models.TextField() #Patient's medical hx or diabetes dx...may change to uniique ID in place of this  though
+
+    def __str__(self):
+        return self.name
+
