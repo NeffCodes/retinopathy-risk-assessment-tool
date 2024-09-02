f57e58571c4cbc3a89371f963787e67db3a5c58d
James Neff <james.m.neff@gmail.com>
2024-09-02 13:44:56 -0400

    setup basic display of patients to dashboard

--- rrat/patients/views.py
+++ rrat/patients/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Patient
 
 # Create your views here.
 def patients_list(request):
-  return render(request, 'patients/patients_list.html')
+  patients = Patient.objects.all().order_by('last_name')
+  return render(request, 'patients/patients_list.html', { 'patients': patients })
