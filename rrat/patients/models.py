f878560d508a398b10008de6d7c06dfc95106268
James Neff <james.m.neff@gmail.com>
2024-09-02 12:55:37 -0400

    update patient model

--- rrat/patients/models.py
+++ rrat/patients/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=75)
+    last_name = models.CharField(max_length=75)
+    date_of_birth = models.DateField(max_length=8)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    @property
+    def age(self):
+        """Returns the person's current age"""
+        from datetime import date
+        today = date.today()
+        born = self.date_of_birth
+        age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+        return age
+
+    @property
+    def full_name(self):
+        """Returns the person's full name separated by a comma"""
+        return f"{self.last_name}, {self.first_name}"
