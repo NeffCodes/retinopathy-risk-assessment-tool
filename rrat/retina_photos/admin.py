01290ed3640c79e9819508673ae7c1aebe430205
James Neff <james.m.neff@gmail.com>
2024-10-14 14:46:44 -0400

    register new app

--- rrat/retina_photos/admin.py
+++ rrat/retina_photos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import RetinaPhoto as RetinaPhotoModel
 
+class RetinaPhotoAdmin(admin.ModelAdmin):
+    # What shows up on the admin panel list
+    list_display = ("id")
+
+    # what shows up when you edit or add a patient via the admin panel
+    fields = ("id")
+
+    
 # Register your models here.
+admin.site.register(RetinaPhotoModel, RetinaPhotoAdmin)
