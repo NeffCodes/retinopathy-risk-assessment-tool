336b9abd9e05e5f2ad98bcfcb28659ab5a791674
James Neff <james.m.neff@gmail.com>
2024-10-14 14:43:31 -0400

    setup basic model

--- rrat/retina_photos/models.py
+++ rrat/retina_photos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class RetinaPhoto(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False,
+        unique=True
+    )
+    
+    def __str__(self):
+        return self.id
