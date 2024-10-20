c8276d0ea10dd0b4c92fdf7316a6e423b1f17c27
James Neff <72764232+NeffCodes@users.noreply.github.com>
2024-10-20 12:44:19 -0400

    Resolves #19 - Setup Retina Photo Model (#28)
    * Create new Retina Photos app
    
    * setup basic model
    
    * connect retina photo app to rrat
    
    * register new app
    
    * update retina photo admin to be tuple
    
    * setup basic for template tags
    
    * WIP - updating photo model
    
    * need to potentially break choices into enum
    * need to finish adding cloudinary public id
    * need to add result field with choices
    
    * break choices out as text choice enums
    
    * add cloudinary public id
    
    * manually set patient_id column name and table name
    
    * update admin panel to display model
    
    * add image tag to model to display on admin panel

--- rrat/retina_photos/admin.py
+++ rrat/retina_photos/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from .models import RetinaPhoto as RetinaPhotoModel
 
 class RetinaPhotoAdmin(admin.ModelAdmin):
+    readonly_fields = ("image","position","cloudinary_public_id", 'image_tag')
+
     # What shows up on the admin panel list
-    list_display = ("id",)
+    list_display = ("patient_name", "position", "cloudinary_public_id")
 
     # what shows up when you edit or add a patient via the admin panel
-    fields = ("id",)
+    fields = ("patient", "image", "position", "cloudinary_public_id", "image_tag")
 
-    
+    # Pull the name of the patient through the foreign key relationship
+    def patient_name(self, obj):
+        return obj.patient.full_name
+    patient_name.short_description = 'Patient Name'
+
+
 # Register your models here.
 admin.site.register(RetinaPhotoModel, RetinaPhotoAdmin)
