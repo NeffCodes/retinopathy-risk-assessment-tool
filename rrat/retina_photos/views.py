05342d23c665360b56bcf663eb029f112ea329b2
James Neff <james.m.neff@gmail.com>
2024-10-25 11:36:28 -0400

    setup basic delete

--- rrat/retina_photos/views.py
+++ rrat/retina_photos/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import get_object_or_404
+from .models import RetinaPhoto as RetinaPhotoModel
 from .forms import RetinaForm
 from .utils import upload_cloudinary_retina
-
+from patients.cloudinary_helpers import destroy_cloudinary_image
 def form_add_new_retina_photo(request, patient):
     """
     Logic to add new retina photo to cloudinary db
@@ -35,4 +37,28 @@ def delete_retina_photo(request, id):
     """
     Hard deletes the image from both the database and from cloudinary.
     """
-    pass
+    
+    # retrieve photo from the local db
+    db_image = get_object_or_404(RetinaPhotoModel, id=id)
+
+    # get cloudinary pubilc ID
+    cloudinary_public_id = db_image.cloudinary_public_id
+    print(f"Public ID: {cloudinary_public_id}")
+
+    try:
+        # delete from cloudinary
+        cloud = destroy_cloudinary_image(cloudinary_public_id)
+
+        if cloud.get('result') == 'not found':
+            print("Image not found in Cloudinary. Check the public ID and folder structure.")
+
+        if cloud.get('result') == "error":
+            print("Error trying to delete the image on cloudinary:")
+            print(cloud.message)
+        
+        # delete from local db if successful
+        if cloud.get('result') == 'ok':
+            db_image.delete()
+            print("Retina photo deleted")
+    except Exception as e:
+        print(f"Error trying to delete image in Cloudinary: {e}")
