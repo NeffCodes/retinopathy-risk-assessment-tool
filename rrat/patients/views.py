6417fb2b6d39f0f93d8a10f32ec7149441664224
SNMeans <suminmeans@gmail.com>
2024-10-26 17:54:20 -0400

    Update About page for Diabetic Retinopathy with detailed information on the condition, its management, and prognosis

--- rrat/patients/views.py
+++ rrat/patients/views.py
@@ -1,133 +1,137 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required  # Import the login_required decorator
 from .models import Patient as PatientModel
 from .forms import PatientForm
 from .decorators import check_patient_hidden
 import cloudinary.uploader
 from .cloudinary_helpers import *
 
+@login_required(login_url='login')  # Protect the patients_list view
 def patients_list(request):
-  """
-  Patient page to view a full list of patients which also has a section to add new patients.
-  """
-  context = {}
+    """
+    Patient page to view a full list of patients which also has a section to add new patients.
+    """
+    context = {}
 
-  # create an object of the patient form to create a new patient
-  form = PatientForm(request.POST or None, request.FILES or None)
+    # create an object of the patient form to create a new patient
+    form = PatientForm(request.POST or None, request.FILES or None)
 
-  # check if form data is valid
-  if form.is_valid():
-    # get data from form instance
-    patient_instance = form.save(commit=False) # don't save the form yet
+    # check if form data is valid
+    if form.is_valid():
+        # get data from form instance
+        patient_instance = form.save(commit=False)  # don't save the form yet
 
-    # If instance has an image
-    if request.FILES.get('avatar'):
-      image_file = request.FILES['avatar']
+        # If instance has an image
+        if request.FILES.get('avatar'):
+            image_file = request.FILES['avatar']
 
-      # Set the new public ID 
-      set_cloudinary_public_id(patient_instance)
+            # Set the new public ID 
+            set_cloudinary_public_id(patient_instance)
 
-      # Upload the image to Cloudinary with transformations
-      result = upload_cloudinary_avatar(image_file, patient_instance.cloudinary_public_id, 'rrat/avatars')
+            # Upload the image to Cloudinary with transformations
+            result = upload_cloudinary_avatar(image_file, patient_instance.cloudinary_public_id, 'rrat/avatars')
 
-      # Set the image URL to the patient instance
-      patient_instance.avatar = result['url']
+            # Set the image URL to the patient instance
+            patient_instance.avatar = result['url']
 
-    # Save patient
-    patient_instance.save()
-    return redirect("patients:list")
-  else:
-    print(form.errors)
+        # Save patient
+        patient_instance.save()
+        return redirect("patients:list")
+    else:
+        print(form.errors)
 
-  # get all patients in db that are not hidden
-  patients = PatientModel.objects.all().exclude(hidden=True).order_by('last_name')
+    # get all patients in db that are not hidden
+    patients = PatientModel.objects.all().exclude(hidden=True).order_by('last_name')
 
-  # set context
-  context['form'] = form
-  context['patients'] = patients
+    # set context
+    context['form'] = form
+    context['patients'] = patients
 
-  return render(request, 'patients/patients_list.html', context)
+    return render(request, 'patients/patients_list.html', context)
 
+@login_required(login_url='login')  # Protect the view_patient view
 @check_patient_hidden
 def view_patient(request, id):
-  """
-  Patient page to view patient details.
-  """
-  patient = PatientModel.objects.get(id=id)
-  return render(request, 'patients/view_patient.html', { 'patient': patient })
+    """
+    Patient page to view patient details.
+    """
+    patient = get_object_or_404(PatientModel, id=id)
+    return render(request, 'patients/view_patient.html', {'patient': patient})
 
+@login_required(login_url='login')  # Protect the update_patient view
 @check_patient_hidden
 def update_patient(request, id):
-  """
-  Patient page to change patient details
-  """
-  context = {}
+    """
+    Patient page to change patient details
+    """
+    context = {}
 
-  # fetch the object related to the provided patient id
-  patient = get_object_or_404(PatientModel, id=id)
+    # fetch the object related to the provided patient id
+    patient = get_object_or_404(PatientModel, id=id)
 
-  # pass the patient object as an instance into the form
-  form = PatientForm(request.POST or None, request.FILES or None, instance = patient)
+    # pass the patient object as an instance into the form
+    form = PatientForm(request.POST or None, request.FILES or None, instance=patient)
 
+    # validate and save data from the form and redirect to patient_view
+    if form.is_valid():
+        # get data from form instance
+        patient_instance = form.save(commit=False)  # don't save the form yet
 
-  # validate and save data from the form and redirect to patient_view
-  if form.is_valid():
-    # get data from form instance
-    patient_instance = form.save(commit=False) # don't save the form yet
+        # If instance has an image, update image info
+        if request.FILES.get('avatar'):
+            image_file = request.FILES['avatar']
 
-    # If instance has an image, update image info
-    if request.FILES.get('avatar'):
-      image_file = request.FILES['avatar']
+            # Remove old image
+            cloud = destroy_cloudinary_image(patient_instance.cloudinary_public_id)
 
-      # Remove old image
-      cloud = destroy_cloudinary_image(patient_instance.cloudinary_public_id)
-                
-      if cloud.get('result') == 'not found':
-          print("Image not found in Cloudinary. Check the public ID and folder structure.")
+            if cloud.get('result') == 'not found':
+                print("Image not found in Cloudinary. Check the public ID and folder structure.")
 
-      if cloud.get('result') == "error":
-          print("Error trying to delete the image on cloudinary:")
-          print(cloud.message)
+            if cloud.get('result') == "error":
+                print("Error trying to delete the image on cloudinary:")
+                print(cloud.message)
 
-      if cloud.get('result') == 'ok':
-        # Set a new public ID 
-        set_cloudinary_public_id(patient_instance)
+            if cloud.get('result') == 'ok':
+                # Set a new public ID 
+                set_cloudinary_public_id(patient_instance)
 
-        # Upload the new image to Cloudinary
-        result = upload_cloudinary_avatar(image_file,patient_instance.cloudinary_public_id,'rrat/avatars')
+                # Upload the new image to Cloudinary
+                result = upload_cloudinary_avatar(image_file, patient_instance.cloudinary_public_id, 'rrat/avatars')
 
-        # Set the image URL to the patient instance
-        patient_instance.avatar = result['url']
+                # Set the image URL to the patient instance
+                patient_instance.avatar = result['url']
 
-    # Save patient
-    patient_instance.save()
-    return redirect("patients:patient_view", id)
-  else:
-    print(form.errors)
+        # Save patient
+        patient_instance.save()
+        return redirect("patients:patient_view", id)
+    else:
+        print(form.errors)
 
-  # set context
-  context["form"] = form
-  context["patient"] = patient
+    # set context
+    context["form"] = form
+    context["patient"] = patient
 
-  return render(request, 'patients/update_patient.html', context)
+    return render(request, 'patients/update_patient.html', context)
 
+@login_required(login_url='login')  # Protect the delete_patient view
 @check_patient_hidden
 def delete_patient(request, id):
-  """
-  Soft deletes patient from database by marking the hidden field as true.
-  """
-  context = {}
+    """
+    Soft deletes patient from database by marking the hidden field as true.
+    """
+    context = {}
 
-  # retrieve patient
-  patient = get_object_or_404(PatientModel, id = id)
+    # retrieve patient
+    patient = get_object_or_404(PatientModel, id=id)
 
-  # if confirmation modal approved
-  if request.method == "POST":
-    # mark as hidden and return to list
-    patient.hidden = True
+    # if confirmation modal approved
+    if request.method == "POST":
+        # mark as hidden and return to list
+        patient.hidden = True
 
-    # update the db and redirect to patient list dashboard
-    patient.save(update_fields=["hidden"])
-    return redirect("patients:list")
+        # update the db and redirect to patient list dashboard
+        patient.save(update_fields=["hidden"])
+        return redirect("patients:list")
   
-  context["patient"] = patient
-  return render(request, "patients/delete_patient.html", context)
+    context["patient"] = patient
+    return render(request, "patients/delete_patient.html", context)
--- rrat/users/views.py
+++ rrat/users/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render, redirect 
-from django.contrib.auth.forms import UserCreationForm 
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+# Registration view
 def register_view(request):
-    if request.method == "POST": 
-        form = UserCreationForm(request.POST) 
-        print(form)
-        if form.is_valid(): 
-            form.save() 
-            return redirect("/")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user after registration
+            return redirect("/")  # Redirect to home or desired page after registration
     else:
         form = UserCreationForm()
-    
-    args = { "form": form }
+
+    args = {"form": form}
     return render(request, "users/register.html", args)
+
+# Login view
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)  # Log the user in
+                return redirect("/")  # Redirect to home or desired page after login
+    else:
+        form = AuthenticationForm()
+
+    args = {"form": form}
+    return render(request, "users/login.html", args)
