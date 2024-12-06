a8aadd48077d3eefb65eb9b4e4e6c4f9eac60aad
James Neff <james.m.neff@gmail.com>
2024-12-06 14:09:19 -0500

    chore: format utils

--- rrat/retina_photos/utils.py
+++ rrat/retina_photos/utils.py
@@ -2,20 +2,24 @@ from django.utils.timezone import now
 import cloudinary.uploader
 from .choices import PrognosisChoices
 
+
 def set_retina_cloudinary_public_id(image_instance):
     """
-    This function will set the cloudinary public id for a retina image instance. 
+    This function will set the cloudinary public id for a retina image instance.
     String will include the current time.
     Returns nothing.
     """
     date_str = now().strftime("%Y-%m-%d--%H:%M")
-    
-    custom_public_id = f"{image_instance.position}--{str(image_instance.id)}--{date_str}"
+
+    custom_public_id = (
+        f"{image_instance.position}--{str(image_instance.id)}--{date_str}"
+    )
 
     image_instance.cloudinary_public_id = custom_public_id
     return image_instance.cloudinary_public_id
 
-def upload_cloudinary_retina(image_file, image_instance, folder= 'rrat/retina_photos'):
+
+def upload_cloudinary_retina(image_file, image_instance, folder="rrat/retina_photos"):
     """
     This function will transform and upload a retina photo to cloudinary
     """
@@ -26,7 +30,13 @@ def upload_cloudinary_retina(image_file, image_instance, folder= 'rrat/retina_ph
             file=image_file,
             public_id=public_id,
             transformation=[
-                {"width": 256, "height": 256, "crop": "fill", "quality": "auto", "gravity": "auto"}
+                {
+                    "width": 256,
+                    "height": 256,
+                    "crop": "fill",
+                    "quality": "auto",
+                    "gravity": "auto",
+                }
             ],
             folder=folder,
         )
@@ -34,6 +44,7 @@ def upload_cloudinary_retina(image_file, image_instance, folder= 'rrat/retina_ph
         return cloud
     except Exception as e:
         print(f"\n+===== Image upload error: {e}\n")
+
 
 def destroy_cloudinary_retina_image(public_id):
     """
@@ -48,6 +59,7 @@ def destroy_cloudinary_retina_image(public_id):
     print(f"= Cloudinary Destroy Response: {public_id} {cloud}")
     return cloud
 
+
 def hard_delete_image_from_all_db(image_obj):
     """
     This function trys to delete an image from both cloudinary and local db
@@ -60,16 +72,17 @@ def hard_delete_image_from_all_db(image_obj):
         # delete from cloudinary
         cloud = destroy_cloudinary_retina_image(cloudinary_public_id)
 
-        if cloud.get('result') == 'not found':
-            print("Image not found in Cloudinary. Check the public ID and folder structure.")
+        if cloud.get("result") == "not found":
+            print(
+                "Image not found in Cloudinary. Check the public ID and folder structure."
+            )
 
-        if cloud.get('result') == "error":
+        if cloud.get("result") == "error":
             print("Error trying to delete the image on cloudinary:")
             print(cloud.message)
 
-        
         # delete from local db if successful
-        if cloud.get('result') == 'ok':
+        if cloud.get("result") == "ok":
             image_obj.delete()
             print("Retina photo deleted")
 
@@ -78,17 +91,32 @@ def hard_delete_image_from_all_db(image_obj):
     except Exception as e:
         print(f"Error trying to delete image in Cloudinary: {e}")
 
+
 def get_prognosis_choice(api_result: int) -> str | None:
-    if not isinstance(api_result, int) or api_result < 0 or api_result > 4:
+    """
+    Maps an integer API result to a prognosis choice string.
+
+    Args:
+        api_result (int): An integer representing the result from an API call.
+
+    Returns:
+        str | None: The corresponding prognosis choice if the api_result is valid;
+        otherwise, returns None if the api_result is invalid.
+    """
+    API_RESULT_MAPPING = [
+        PrognosisChoices.NORMAL,
+        PrognosisChoices.MILD,
+        PrognosisChoices.MODERATE,
+        PrognosisChoices.SEVERE,
+        PrognosisChoices.PROLIFERATIVE,
+    ]
+
+    if (
+        not isinstance(api_result, int)
+        or api_result < 0
+        or api_result > len(API_RESULT_MAPPING) - 1
+    ):
         print(f"Oops, Invalid API result: {api_result}")
         return None
-
-    API_RESULT_MAPPING = {
-        0:PrognosisChoices.NORMAL,
-        1:PrognosisChoices.MILD,
-        2:PrognosisChoices.MODERATE,
-        3:PrognosisChoices.SEVERE,
-        4:PrognosisChoices.PROLIFERATIVE
-    }
 
     return API_RESULT_MAPPING[api_result]
